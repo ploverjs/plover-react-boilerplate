217fd96b4de2140eb22ffd8b6d7b5ad0cb32e6c0
bencode <bencode@163.com>
2017-06-30 01:23:46 +0800

    update

--- config/app.js
+++ config/app.js
@@ -1,9 +1,18 @@
+const pathUtil = require('path');
+
+
 /**
  * 服务器相关配置
  */
 exports.server = {
   port: 4000
 };
+
+
+/**
+ * 模块目录
+ */
+exports.modulesDir = pathUtil.join(__dirname, '../app');
 
 
 /**
@@ -27,11 +36,14 @@ exports.web = {
   // 用于设置app.keys, 实际应用需要重新产生一个
   keys: ['17e6b6bc6129097383dcad4fa1602233'],
 
-  // https://github.com/koajs/favicon
-  // favicon: pathUtil.join(__dirname, '../public/favicon.ico'),
-
   // https://github.com/koajs/response-time
-  rtime: {}
+  rtime: {},
+
+  favicon: pathUtil.join(__dirname, '../public/favicon.ico'),
+
+  static: {
+    root: pathUtil.join(__dirname, '../public')
+  }
 };
 
 
@@ -39,5 +51,10 @@ exports.web = {
  * 前端资源相关配置
  */
 exports.assets = {
-  simpleMode: true
+  // 只使用简单模式，前端资源主要由webpack处理
+  simple: true,
+  // 对资源进行hash重命名，适合发布到CDN等场景
+  digest: true,
+  // 资源前缀
+  prefix: '/priv'
 };
